e02bda111e0b64e32e02598c3dbba9e320538992
shawn <shawn@frozen.ca>
2023-01-18 22:40:46 -0600

    Removed Description and Units.

--- GpsItem.cs
+++ GpsItem.cs
@@ -6,27 +6,23 @@ namespace FrozenNorth.Gpmf
 	/// <summary>
 	/// A GPS item read from GPMF data.
 	/// </summary>
-	public class GpsItem
+	internal class GpsItem
 	{
 		// instance variables
 		public GpsCoords Coords;
-		public string Description;
 		public DateTime Time;
 		public double Precision;
 		public uint Fix;
-		public string[] Units;
 
-		/// <summary>
-		/// Initializes the fields from encoded values.
-		/// </summary>
-		/// <param name="gps">Array of GPS values.</param>
-		/// <param name="scale">Array of scale values to apply to the GPS values.</param>
-		/// <param name="description">Description.</param>
-		/// <param name="time">Time of the first GPS point.</param>
-		/// <param name="precision">Precision of the data.</param>
-		/// <param name="fix">Fix value.</param>
-		/// <param name="units">Units of the GPS values.</param>
-		public GpsItem(int[] gps, int[] scale, string description, DateTime time, double precision, uint fix, string[] units)
+        /// <summary>
+        /// Initializes the fields from encoded values.
+        /// </summary>
+        /// <param name="gps">Array of GPS values.</param>
+        /// <param name="scale">Array of scale values to apply to the GPS values.</param>
+        /// <param name="time">Time of the first GPS point.</param>
+        /// <param name="precision">Precision of the data.</param>
+        /// <param name="fix">Fix value.</param>
+        internal GpsItem(int[] gps, int[] scale, DateTime time, double precision, uint fix)
 		{
 			Coords = new GpsCoords();
 			for (int i = 0; i < gps.Length; i += 5)
@@ -35,21 +31,14 @@ namespace FrozenNorth.Gpmf
 												gps[i + 3] * 1.0 / scale[3], gps[i + 4] * 1.0 / scale[4]);
 				Coords.Add(coord);
 			}
-			Description = description;
 			Time = time;
 			Precision = precision;
 			Fix = fix;
-			Units = units;
 		}
-
-		/// <summary>
-		/// Gets the units as a comma separated string.
-		/// </summary>
-		public string UnitsString => string.Join(",", Units);
 	}
 
-	/// <summary>
-	/// A list of GPS items read from GPMF data.
-	/// </summary>
-	public class GpsItems : List<GpsItem> { }
+    /// <summary>
+    /// A list of GPS items read from GPMF data.
+    /// </summary>
+    internal class GpsItems : List<GpsItem> { }
 }
